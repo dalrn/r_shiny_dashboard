10688e5c25880c726d3a550ae156f5533e68ea1b
Andalan <andalan7676@gmail.com>
2025-11-19 19:19:56 +0700

    benerin wafi

--- app/app.R
+++ app/app.R
@@ -2,11 +2,11 @@ packages <- c("shiny", "shinythemes", "forecast", "ggplot2", "dplyr")
 library(shiny)
 
 # source semua komponen
-source("r_shiny_dashboard/app/ui.R")
-source("r_shiny_dashboard/app/server.R")
-source("r_shiny_dashboard/app/ts_decomp.R")
-source("r_shiny_dashboard/app/forecast.R")
-source("r_shiny_dashboard/app/diagnostics.R")
+source("app/ui.R")
+source("app/server.R")
+source("app/ts_decomp.R")
+source("app/forecast.R")
+source("app/diagnostics.R")
 
 # jalankan aplikasi
 shinyApp(ui = ui, server = server)
